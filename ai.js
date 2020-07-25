8bc7500754f879bf6a8e5a3338e3dcd0223c3246
REX_HOLMSTROM <rexholmstrom96@gmail.com>
2020-07-26 00:28:44 +0200

    first commit

---
+++ ai.js
@@ -0,0 +1,22 @@
+var origBoard;
+const huPlayer = '0';
+const aiPlayer = 'X';
+//winning combos available 
+const winCombos = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [6, 4, 2]
+]
+
+const cells = document.querySelectorAll('.cell');
+//Function to start the game
+startGame();
+
+function startGame() {
+    document.querySelector(".endgame").style.display = "none";
+}
